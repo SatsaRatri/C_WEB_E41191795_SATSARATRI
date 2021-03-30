f1299e58e5a502d17d65eb9c382722646133c1eb
SatsaRatri <bernadeta252@gmail.com>
2021-03-30 18:30:53 +0700

    Membuat  Vie Blade 3/3

--- Tugas3/resources/views/home.blade.php
+++ Tugas3/resources/views/home.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('','', app() -> getLocale()) }}">
- 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-widht, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title> {{ config('app.name') }}</title>
-    </head>
-    <body>
-        <div class="jumbotron jumbroton-fluid">
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron jumbotron-fluide">
         <div class="container">
-            <h1 class="display-4">HALAMAN HOME</h1>
-            <p class="lead">Halaman ini merupakan halaman home.</p>
+            <h1 class="display-4">HOME PAGE</h1>
+            <p class="lead">This is the one Home Page</p>
         </div>
-        </div>
-    </body>
-</html>
+    </div>
