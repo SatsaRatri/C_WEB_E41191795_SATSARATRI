f7ab48ae4ee3e1914198a49a032cd1987de60d3d
Satsa Ratri <71832277+SatsaRatri@users.noreply.github.com>
2021-06-14 19:15:28 +0700

    3/2 Membuat Model View Controller Pada Laravel

---
+++ Minggu3/app/Http/Controllers/DetailProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DetailProfileController extends Controller
+{
+  public function index(){
+      return "Hallo";
+  }
+  public function create(){
+      return "Create";
+  }
+  public function store(Request $request){
+      return "Menciptakan User Baru";
+  }
+  public function show($id){
+    return "Mengambil Satu Data User" .$id;
+  }
+  public function edit($id){
+    return "Mengubah Satu Data User" .$id;
+  }
+  public function update(Request $request, $id){
+    return "Mengubah Data User Dengan id" . $id;
+  }
+  public function destroy($id){
+    return "Menghapus Data Dengan id" .$id;
+  }
+}
