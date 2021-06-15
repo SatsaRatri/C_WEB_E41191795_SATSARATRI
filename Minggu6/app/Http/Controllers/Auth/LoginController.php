8517c7122a027b11f2d652cfe2dddde692a14002
Satsa Ratri <71832277+SatsaRatri@users.noreply.github.com>
2021-06-15 09:02:07 +0700

    Membuat Auth Login Username Pada Laravel Minggu 6/1

--- Minggu6/app/Http/Controllers/Auth/LoginController.php
+++ Minggu6/app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,22 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function login (Request $request){
+        $this->validate($request, [
+            'username'=> 'required|string',
+            'password'=> 'required|string|min:6',
+        ]);
+         
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email':'username';
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($login)){
+            return redirect ()->route('home');
+        }
+            return redirect ()->route('login')->with ([error => 'Email/Password Salah']);
+    }
+
 }
