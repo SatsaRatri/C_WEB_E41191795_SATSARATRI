cfe1d3a98d041a87c5edb85e1973c20a1e17f340
Satsa Ratri <71832277+SatsaRatri@users.noreply.github.com>
2021-06-16 16:49:27 +0700

    Membuat CRUD (Query Builder) Minggu 7/4

---
+++ Minggu7/database/migrations/2021_04_22_033101_create_pendidikan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendidikanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendidikan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->tinyInteger('tingkatan');
+            $table->year('tahun_masuk');
+            $table->year('tahun_lulus');
+            $table->integer('userID')->unsigned()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendidikan');
+    }
+}
